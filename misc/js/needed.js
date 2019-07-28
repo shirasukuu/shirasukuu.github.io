9bb65ab2f5dda13bfd559fe2ce0c3a435bff6c2b
shirasukuu <52996539+shirasukuu@users.noreply.github.com>
2019-07-28 20:40:07 +0900

    changes

--- misc/js/needed.js
+++
@@ -1,4 +0,0 @@
-﻿        $("head").append('<style>.box { opacity: 0;-ms-filter:"alpha( opacity=0 )";filter: alpha( opacity=0 ); }</style>');
-        $(function() {
-            $(".box").fadeMover();
-        });

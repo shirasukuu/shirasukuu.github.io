533871a595327501f7706695d9d24ebaa24e9ea4
shirasukuu <52996539+shirasukuu@users.noreply.github.com>
2019-07-28 09:53:42 +0900

    create misc folder.
    for auther & shirasu misc.html was made.

---
+++ misc/js/option.js
@@ -0,0 +1,11 @@
+﻿        $(function() {
+            $('body').fadeMover({
+                'effectType': 1,
+                'inSpeed': 1000, <!-- fade out time -->
+                'outSpeed': 1000,
+                'inDelay': '0',
+                'outDelay': '0',
+                'nofadeOut': 'nonmover'
+            });
+        });
+  
